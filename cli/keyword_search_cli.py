bc2cd427daee9ba862bc89d090f93a538a73f556
tobi1010 <dev.tobi1010@gmail.com>
2026-03-21 00:21:20 +0100

    nlp complete

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import argparse
+import string
 import json
 from pathlib import Path
+from nltk.stem import PorterStemmer
 
 
 def main() -> None:
@@ -19,7 +21,7 @@ def main() -> None:
             result = exec_search(args.query)
             print(f"Searching for: {args.query}")
             for i, movie in enumerate(result, start=1):
-                print(f"{i}. {movie["title"]}")
+                print(f"{i}. {movie['title']}")
             pass
         case _:
             parser.print_help()
@@ -27,15 +29,33 @@ def main() -> None:
 
 def exec_search(query):
 
+    s = process_string(query)
     path = Path("~/dev/rag-search-engine/data/movies.json").expanduser()
     with path.open() as fp:
         data = json.load(fp)
     result = []
     for movie in data["movies"]:
-        if query in movie["title"]:
+        title_tokens = process_string(movie["title"])
+        if any(
+            query_token in title_token
+            for title_token in title_tokens
+            for query_token in s
+        ):
             result.append(movie)
     result.sort(key=lambda movie: movie["id"])
     return result[:5]
+
+
+def process_string(s):
+    path = Path("~/dev/rag-search-engine/data/stopwords.txt").expanduser()
+    with path.open() as fp:
+        STOPWORDS = fp.read().splitlines()
+    s = s.lower()
+    no_punct = s.translate(str.maketrans("", "", string.punctuation))
+    tokens = [t for t in no_punct.split() if t not in STOPWORDS]
+    stemmer = PorterStemmer()
+    tokens = [stemmer.stem(token) for token in tokens]
+    return tokens
 
 
 if __name__ == "__main__":
